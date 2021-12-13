71115225c4e522c287e87832515ce6f15b39022d
Amr Monier <a.monier2107@gmail.com>
2021-12-13 21:02:29 +0200

    added socket.io to the app

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
+import http from "http";
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import path from "path";
+import {fileURLToPath} from "url";
+import {dirname} from "path"
+import {Server} from "socket.io"
 dotenv.config()
-//import routes from './routes';
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 class App {
   constructor(port) {
@@ -16,22 +23,29 @@ class App {
   middlewares() {
     this.server.use(bodyParser.json());
     this.server.use(cors())
+    this.server.use(express.static(path.join(__dirname, 'public')))
   }
 
   routes() {
     //this.server.use(routes);
+    this.server.get('/', (req, res, next) => {
+        res.redirect('/client')
+    })
   }
 
   run() {
-    this.server.listen(this.port, (err) => {
-      if (err) {
-        return console.log(err);
-      }
-      console.log(`App Running on http://localhost:${this.port}`);
-    });
+    const server = http.createServer(this.server)
+    const io = new Server(server)
+    io.on('connection', (socket) => {
+        console.log( 'connected')
+    })
+    server.listen(this.port, () => {
+        console.log(`App Running on http://localhost:${server.address().port}`);
+      })
+  
   }
 }
 
-const server = new App(3030);
+const app = new App(3030);
 
-server.run();
+app.run();
